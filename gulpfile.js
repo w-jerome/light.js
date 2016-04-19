08ed726cae296bfaedc901cc0ee26c6f105df28a
Jérôme <jerome@studiometa.fr>
2016-04-19 13:25:58 +0200

    Ajout le workflow pour la page de présentation

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp         = require('gulp');
+var sass         = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+// Sass options
+var sassOptions = {
+	src  : 'scss/**/*.scss',
+	dest : 'css',
+};
+
+// Autoprefixer options
+var autoprefixerOptions = {
+	browsers : ['last 4 versions', '> 0.2%', 'Firefox ESR']
+};
+
+// Sass compilation
+gulp.task('sass', function () {
+  return gulp
+    .src(sassOptions.src)
+    .pipe(sass())
+    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(gulp.dest(sassOptions.dest));
+});
+
+// Watch
+gulp.task('watch', ['sass'], function() {
+	gulp.watch(sassOptions.src, ['sass']);
+});
+
+
+gulp.task('default', ['watch']);
