fe03bbcc7d21b372d198bcbf27be7dec3be7fb6a
Jérôme <jerome@studiometa.fr>
2016-04-20 14:31:38 +0200

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp         = require('gulp');
 var sass         = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var plumber 	 = require('gulp-plumber');
 
 // Sass options
 var sassOptions = {
@@ -17,6 +18,7 @@ var autoprefixerOptions = {
 gulp.task('sass', function () {
   return gulp
     .src(sassOptions.src)
+	.pipe(plumber())
     .pipe(sass())
     .pipe(autoprefixer(autoprefixerOptions))
     .pipe(gulp.dest(sassOptions.dest));
